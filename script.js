9451ed39c8d1eec8bdf868c550177a6671f50087
Ana-Seidel <anaafsw1@gmail.com>
2024-08-10 17:41:09 -0300

    fix details

--- script.js
+++ script.js
@@ -15,3 +15,17 @@ document.addEventListener('DOMContentLoaded', () => {
         observer.observe(element);
     });
 });
+
+//Função para saber em qual página está
+document.addEventListener("DOMContentLoaded", function() {
+    var links = document.querySelectorAll('.cabecalho a');
+    var currentPage = window.location.pathname.split('/').pop(); // Obtém o nome do arquivo da URL
+
+    links.forEach(function(link) {
+        if (link.getAttribute('href') === currentPage) {
+            link.classList.add('active');
+        } else {
+            link.classList.remove('active');
+        }
+    });
+});
